d94dbba87215a9c4761f8167bf25661c8dd597a0
atlehjelmseth <atle.hjelmseth@stud.noroff.no>
2025-01-28 22:46:36 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <p>
+          Start with this 2
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
